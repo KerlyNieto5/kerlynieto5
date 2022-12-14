8a5a2134b030e4c812440ea4751bcca0fad242b9
Kerly Vanesa Nieto Mora <116193228+KerlyNieto5@users.noreply.github.com>
2022-12-14 08:56:08 -0500

    Delete condicional1.py

--- condicional1.py
+++
@@ -1,8 +0,0 @@
-x =int (input('digite el primer numero'))
-y =int (input('digite el segundo numero'))
-z =int (input('digite el tercer numero'))
-
-menor = min(x,y,z)
-mayor = max(x,y,z)
-valor_medio =( x + y + z) - menor - mayor
-print ("el valor medio es:", valor_medio)
